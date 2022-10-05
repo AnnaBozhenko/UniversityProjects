4813dd71556695ae43aaa19fa6e62a4759f8922f
Anna Bozhenko <ms.anna.bozhenko.03@gmail.com>
2022-10-05 23:35:01 +0300

    Simplified forms of queries were made.

--- data_base/5_semester/lab_1/queries_3rd_part.sql
+++ data_base/5_semester/lab_1/queries_3rd_part.sql
@@ -1,29 +1,28 @@
 -- Яка назва проданого найдорожчого товару?
 
 -- select 
--- 	distinct prod_name 
--- 	from products inner join orderitems on products.prod_id = orderitems.prod_id  
--- 	where item_price = (select max(item_price) from orderitems)
+	-- distinct prod_name 
+	-- from products 
+	-- inner join orderitems using(prod_id)  
+-- where item_price = (select 
+						-- max(item_price)
+						-- from orderitems)
 
 -- Як звуть покупця з найдовшим іменем – поле назвати long_name
 
 -- select 
--- 	cust_name as long_name 
--- 	from customers
+	-- cust_name as long_name 
+	-- from customers
 -- where length(cust_name) = (select 
--- 						   	max(length(cust_name))
--- 						   	from customers)
+						   	-- max(length(cust_name))
+						   	-- from customers)
+
 
 -- Вивести PROD_ID товарів та імена постачальників для тих товарів, 
 -- які були продані. Результат вивести у верхньому регістрі, як єдине
 -- поле products_sold
 
--- select 
--- 	trim(upper(pr_v.prod_id || ' - ' || pr_v.vend_name)) as products_sold 
--- 	from (select 
--- 		  	pr.prod_id, v.vend_name
--- 			from products pr 
--- 		  	inner join vendors v on v.vend_id = pr.vend_id) pr_v
--- where pr_v.prod_id in (select 
--- 					   	prod_id 
--- 					   	from orderitems)
+-- select distinct
+	-- upper(products.prod_id || ' - ' || vend_name) as products_sold
+	-- from orderitems inner join products using(prod_id)
+	-- inner join vendors using(vend_id)
